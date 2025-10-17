516595fc606a86989b2a9a7615cd5e6280d7fef3
ValenDilello <v.dilello@alumno.etec.um.edu.ar>
2025-10-17 12:54:30 -0300

    Issue 6 no terminada

---
+++ ecommerce-frontend/src/pages/About.js
@@ -0,0 +1,5 @@
+import React from 'react';
+function About() {
+  return <h2>About / Contacto (próximamente)</h2>;
+}
+export default About;
---
+++ ecommerce-frontend/src/pages/Cart.js
@@ -0,0 +1,5 @@
+import React from 'react';
+function Cart() {
+  return <h2>Carrito de compras (próximamente)</h2>;
+}
+export default Cart;
